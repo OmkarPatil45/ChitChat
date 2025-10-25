16cd11d1ef87f0a8d2830c491d722062d2dc80bd
Omkar Patil <ompatil5132@gmail.com>
2025-10-25 19:21:52 +0530

    message endpoints functionality added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import { getAllContacts } from '../controllers/message.controller.js'
+import { protectRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
-router.get("/send", (req,res) => {
-    res.send("Send message endpoint ")
-})
+router.get("/contacts", protectRoute,getAllContacts)
+
 
 export default router
