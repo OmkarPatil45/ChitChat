158ed339743f00a413e69ef18a3be06908e476c0
Omkar Patil <ompatil5132@gmail.com>
2025-11-03 20:02:47 +0530

    frontend components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import ChatPage from './pages/ChatPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!</h1>
-    </div>
+  <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+  <div className="absolute inset-0 bg-gradient-to-r from-indigo-900 via-purple-900
+   to-gray-900 bg-[length:400%_400%] animate-[moveGradient_18s_ease_infinite] opacity-30" /> 
+    <Routes>
+      <Route path='/' element={<ChatPage/>} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/signup' element={<SignUpPage/>} />
+      </Routes>
+  </div>
+  
+    
+    
+   
   )
 }
 
