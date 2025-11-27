0e7b783d3f5818c8bce85bccb73fa6834c2657e8
Omkar Patil <ompatil5132@gmail.com>
2025-11-27 13:40:24 +0530

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,13 @@
 import { create } from "zustand"
 import {axiosInstance} from '../lib/axios'
 import toast from "react-hot-toast";
+import { LogIn } from "lucide-react";
 
 export const useAuthStore = create ((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -32,11 +34,37 @@ export const useAuthStore = create ((set) => ({
            
            toast.success("Account created successfully!")
         } catch (error) {
-            const message = error?.response?.data?.message || "Signup failed";
-            toast.error(message);
+            toast.error(error.response.data.message);
         } finally{
             set({isSigningUp: false})
         }
 
+    },
+
+    login : async(data) => {
+        set({isLoggingIn: true})
+        try {
+           const res = await axiosInstance.post("/auth/login", data);
+           set({authUser: res.data});
+           
+           toast.success("Logged in successfully")
+        } catch (error) {
+            
+            toast.error(error.response.data.message);
+        } finally{
+            set({isLoggingIn: false})
+        }
+
+    },
+
+    logout : async(data) => {
+        try {
+           await axiosInstance.post("/auth/logout");
+           set({authUser: null});   
+           toast.success("Logged out successfully");     
+        } catch (error) {
+            toast.error("Logout failed. Please try again.");
+            console.log("Logout failed:", error);
+        }
     }
 }));
