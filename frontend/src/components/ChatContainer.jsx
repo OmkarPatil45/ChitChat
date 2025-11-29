af8401962461b8010c793c521106b19ca624f2f4
Omkar Patil <ompatil5132@gmail.com>
2025-11-30 00:37:04 +0530

    chat container setup done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+import { useChatStore } from '../store/useChatStore'
+import ChatHeader  from './ChatHeader'
+import { useEffect } from 'react'
+import NoChatHistoryPlaceholder from './NoChatHistoryPlaceholder'
+import MessageInput from './MessageInput'
+import MessagesLoadingSkeleton from './MessagesLoadingSkeleton'
 
 function ChatContainer() {
+  const {selectedUser, getMessagesByUserId, messages, isMessagesLoading} = useChatStore();
+  const {authUser} = useAuthStore()
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id)
+  }, [selectedUser, getMessagesByUserId])
+
+
   return (
-    <div>chatContainer</div>
+    <>
+      <ChatHeader />
+      <div className="flex-1 px-3 overflow-y-auto py-8">
+        {messages.length > 0 && !isMessagesLoading ? (
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map(msg => (
+              <div key={msg._id}
+              className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+                <div className={
+                  `chat-bubble relative ${
+                    msg.senderId === authUser._id ? 
+                    "bg-cyan-600 text-white" : "bg-slate-800 text-slate-200"
+                  }`
+                  }>
+                    {msg.image && (
+                      <img src={msg.image} alt="Shared" className='rounded-lg h-48 object-cover' />
+                     )}
+                     {msg.text && <p className="mt-2">{msg.text}</p>}
+                     <p className="text-xs mt-1 opacity-75 flex gap-1">
+                       {/* getting error here */}
+                      {/* {new Date(msg.createdAt).toISOString().slice(11, 16)} */}
+                      {new Date(msg.createdAt).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+                     </p>
+
+                  </div>
+              </div>
+            ))}
+          </div>
+        ) : isMessagesLoading ? <MessagesLoadingSkeleton /> : (
+          <NoChatHistoryPlaceholder name={selectedUser.fullName} />
+        )}
+      </div>
+
+      <MessageInput />
+    </>
   )
 }
 
