57e02f14e5aa3a323381603a378093852a38a27c
Omkar Patil <ompatil5132@gmail.com>
2025-11-30 12:24:37 +0530

    large payload issue solved

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.json()) //req.body
+app.use(express.json({limit: "10mb"})) //req.body
 app.use(cors({ origin:'http://localhost:5173', credentials:true}))
 app.use(cookieParser())
 
