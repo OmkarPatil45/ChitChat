cda093adb0840dfbb32db37503ff3ab58f3634a1
Omkar Patil <ompatil5132@gmail.com>
2025-12-01 17:05:26 +0530

    small update in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,12 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from './lib/db.js';
 import { ENV } from "./lib/env.js"
+import { app, server } from './lib/socket.js';
 
 
 dotenv.config();
 
-const app = express()
+
 
 const PORT = process.env.PORT || 3000
 
@@ -22,7 +23,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     console.log("Server is running on port " +PORT)
     connectDB()
 }
