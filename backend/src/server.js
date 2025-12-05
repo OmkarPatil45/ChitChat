7011b0a46397518aa866e66cd263657fa2e91c84
Omkar Patil <ompatil5132@gmail.com>
2025-12-05 10:43:56 +0530

    small update in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,8 @@ import cors from 'cors'
 import authRoutes from "./routes/auth.route.js" 
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from './lib/db.js';
-import { ENV } from "./lib/env.js"
+import { ENV } from './lib/env.js';
 import { app, server } from './lib/socket.js';
-
 
 dotenv.config();
 
@@ -17,7 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json({limit: "10mb"})) //req.body
-app.use(cors({ origin:'http://localhost:5173', credentials:true}))
+app.use(cors({ origin:ENV.CLIENT_URL, credentials:true}))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
