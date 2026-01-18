2d7f9a404df39a21c9b298c71eb7d29d14660298
Omkar Patil <ompatil5132@gmail.com>
2026-01-18 13:18:43 +0530

    minor changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 import authRoutes from "./routes/auth.route.js" 
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import { app, server } from './lib/socket.js';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,6 +24,11 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+})
 
 server.listen(PORT,() => {
     console.log("Server is running on port " +PORT)
