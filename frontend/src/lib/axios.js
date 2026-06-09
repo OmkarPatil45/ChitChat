ef1703f6dfb6ab08b591f750216025c64950007a
Omkar Patil <ompatil5132@gmail.com>
2026-06-09 11:20:39 +0530

    minor updates

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: import.meta.env.MODE === "production" ? "https://chitchat-dh7g.onrender.com" : "/api",
     withCredentials: true,
 })
