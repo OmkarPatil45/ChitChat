05d79e4d80f9ad6c82dab1b3dd2b2708368c964d
Omkar Patil <ompatil5132@gmail.com>
2026-06-10 13:02:42 +0530

    server creation

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,10 +25,12 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
-})
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "ChitChat Backend Running Successfully"
+  });
+});
 
 server.listen(PORT,() => {
     console.log("Server is running on port " +PORT)
