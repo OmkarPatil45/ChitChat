7a9e160a9aa7ecdc1506da16f32445a9a0eb18f9
Omkar Patil <ompatil5132@gmail.com>
2026-06-10 16:18:38 +0530

    axios instance creation

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,12 @@
-// import axios from 'axios'
-
-// export const axiosInstance = axios.create({
-//     baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-//     withCredentials: true,
-// })
+import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
-  withCredentials: true,
-});
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    withCredentials: true,
+})
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+//   withCredentials: true,
+// });
